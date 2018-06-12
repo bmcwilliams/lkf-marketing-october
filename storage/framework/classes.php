402a33aa9d7cf128965e606d90e1d088fcccb392
Bryan Mack Williams <bjmcwilliams@lkfmarketing.com>
2018-06-12 16:17:26 -0600

    First commit

---
+++ storage/framework/classes.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'october\\demo\\Plugin' => 'plugins/october/demo/Plugin.php',
+  'backend\\Controllers\\auth' => 'modules/backend/controllers/auth.php',
+  'backend\\Controllers\\index' => 'modules/backend/controllers/index.php',
+  'cms\\Controllers\\index' => 'modules/cms/controllers/index.php',
+  'October\\Demo\\Components\\Todo' => 'plugins/october/demo/components/Todo.php',
+  'backend\\Controllers\\media' => 'modules/backend/controllers/media.php',
+  'system\\Controllers\\settings' => 'modules/system/controllers/settings.php',
+);
